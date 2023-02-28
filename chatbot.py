27ce79d3e90ca927200d98a1999a0cf3fddc50cd
KuntaWasonga <wkunta@rocketmail.com>
2023-02-28 22:46:30 +0300

    Added bot route

--- chatbot.py
+++ chatbot.py
@@ -4,3 +4,15 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    user_msg = request.values.get('Body', '').lower()
+    bot_resp = MessagingResponse()
+    msg = bot_resp.message()
+    
+    
+    return str(bot_resp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
