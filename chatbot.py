0fb2c348452647c0b506d9097c615576c2140499
KuntaWasonga <wkunta@rocketmail.com>
2023-03-07 22:43:48 +0300

    Updated chatbot file

--- chatbot.py
+++ chatbot.py
@@ -1,29 +1,27 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
 from flask import Flask, request, render_template, url_for
 import requests
-from twilio.twiml.messaging_response import MessagingResponse
+
+# chat initialization
+model = load_model("chatbot_model.h5")
+intents = json.loads(open("intents.json").read())
+words = pickle.load(open("words.pkl", "rb"))
+classes = pickle.load(open("classes.pkl", "rb"))
 
 app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Nicole Agutu',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'April 21, 2018'
-    },
-    {
-        'author': 'Kunta Handel',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 23, 2018'
-    }
-]
-
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 
 @app.route("/about")
@@ -37,12 +35,71 @@ def search():
 
 
 @app.route('/bot', methods=['POST'])
-def bot():
-    user_msg = request.values.get('Body', '').lower()
-    bot_resp = MessagingResponse()
-    msg = bot_resp.message()
-    
-    return str(bot_resp)
+def chatbot_response():
+    msg = request.form["msg"]
+    #checks is a user has given a name, in order to give a personalized feedback
+    if msg.startswith('my name is'):
+        name = msg[11:]
+        ints = predict_class(msg, model)
+        res1 = getResponse(ints, intents)
+        res =res1.replace("{n}",name)
+    elif msg.startswith('hi my name is'):
+        name = msg[14:]
+        ints = predict_class(msg, model)
+        res1 = getResponse(ints, intents)
+        res =res1.replace("{n}",name)
+    #if no name is passed execute normally
+    else:
+        ints = predict_class(msg, model)
+        res = getResponse(ints, intents)
+    return res
+
+
+# chat functionalities
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+
+def getResponse(ints, intents_json):
+    tag = ints[0]["intent"]
+    list_of_intents = intents_json["intents"]
+    for i in list_of_intents:
+        if i["tag"] == tag:
+            result = random.choice(i["responses"])
+            break
+    return result
 
 
 
