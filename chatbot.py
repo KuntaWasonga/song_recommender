1cffd6e8e5a6b4876bf7fe8c6b396a87f5fbd973
KuntaWasonga <wkunta@rocketmail.com>
2023-03-07 19:12:50 +0300

    Updated chatbot file

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Nicole Agutu',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 21, 2018'
+    },
+    {
+        'author': 'Kunta Handel',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 23, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 @app.route('/bot', methods=['POST'])
 def bot():
