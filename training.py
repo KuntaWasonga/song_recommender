17c7dce3fe399fd94155f0a8af2d8f0aa4355ac8
KuntaWasonga <wkunta@rocketmail.com>
2023-03-08 22:16:24 +0300

    Updated training file

--- training.py
+++ training.py
@@ -27,5 +27,52 @@ for intent in intents['intents']:
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+#ensure no duplicates
+classes = sorted(set(classes))
+
+#save into files
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+#creating numerical values for words to be fed to neural network
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        if word in word_patterns:
+            bag.append(1) if word in word_patterns else bag.append(0)
             
-print(documents)
+        output_row = list(output_empty)
+        output_row[classes.index(document[1])] = 1
+        training.append([bag, output_row])
+        
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+
+#Creating neural network
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation = 'relu'))
+model.add(Dropout(0.5))                    #to prevent overfitting
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation = 'softmax')) #Scales results in output layer for sum to be 1
+          
+#defining stochastic gradient descent optimiser
+sgd = SGD(lr= 0.01, decay = 1e-6, momentum = 0.9, nesterov = True)
+model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'] )
+
+#how to feed data into neural network
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, versbose = 1)
+model.save('chatbotmodel.h5', hist)
