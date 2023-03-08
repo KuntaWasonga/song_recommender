068d352d95554f8bc0a41262e466f12ff79ac55b
KuntaWasonga <wkunta@rocketmail.com>
2023-03-08 09:33:26 +0300

    Updated libraries

--- functionalities.py
+++ functionalities.py
@@ -1,11 +1,10 @@
 import random
-import json
-import pickle
 import numpy as np
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import load_model
+from tensorflow import keras
+from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 
