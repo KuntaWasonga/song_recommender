3a1aa661d61e32351a13641e163e1f525f1288e4
KuntaWasonga <wkunta@rocketmail.com>
2023-03-10 00:55:01 +0300

    Updated chatbot file

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, jsonify
 import requests
 
 from functionalities import *
@@ -6,15 +6,19 @@ from functionalities import *
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
+@app.get("/")
+@app.get("/home")
 def home():
     return render_template('home.html')
 
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    #check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
 
 
 if __name__ == '__main__':
