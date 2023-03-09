8b0def49b5182a484e0ccbc4709ac0991187cc85
KuntaWasonga <wkunta@rocketmail.com>
2023-03-09 09:20:04 +0300

    Updated training file

--- training.py
+++ training.py
@@ -48,7 +48,7 @@ for document in documents:
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
-            
+
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
